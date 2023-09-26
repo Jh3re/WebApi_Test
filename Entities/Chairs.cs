3740fb02b3e2434c881be0544e6452157e2f1bfa
Jh3re <jheremymollo.02@gmail.com>
2023-09-26 17:27:45 -0400

    Entidad silla

---
+++ Entities/Chairs.cs
@@ -0,0 +1,12 @@
+
+
+namespace Entities
+{
+    public class Chairs
+    {
+        public int Id { get; set; }
+        public string Modelo { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+    }
+}
